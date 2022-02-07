3c44ee1c233c9a1a3f582bcf220ff420f1ba9bc9
Akash52 <ac8572611@gmail.com>
2022-02-07 11:08:34 +0530

    Link removed

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
         <h1>Hello World!</h1>
       </header>
     </div>
