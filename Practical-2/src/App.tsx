4e7b10ea6d82ca31805acc94bfe65847a35a1b48
Akash52 <ac8572611@gmail.com>
2022-02-11 00:57:00 +0530

    Initial App Structure Ready

--- Practical-2/src/App.tsx
+++ Practical-2/src/App.tsx
@@ -1,32 +1,16 @@
-import React from 'react';
-import {FC, useEffect, useState} from 'react';
+import React, {FC} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 const App: FC = () => {
-    const [advice, setAdvice] = useState('');
-
-    useEffect(() => {
-        const url =
-            'https://raw.githubusercontent.com/Akash52/awesome-Javascript-collection/master/data.json';
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const json = await response.json();
-                console.log(json.profiles);
-                setAdvice(json.name);
-            } catch (error) {
-                console.log('error', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
     return (
-        <div className="app">
-            <h1>Hello World</h1>
-            <p>{advice}</p>
-            <p>Hello World!</p>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
     );
 };
 
---
+++ Practical-2/src/Pages/Hooks.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const About: FC = () => {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    );
+};
+
+export default About;
