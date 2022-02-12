aad94bcc26d92b276d0fb9310a6ef48d6cecb220
Akash52 <ac8572611@gmail.com>
2022-02-12 17:38:30 +0530

    Profile list implement

--- Practical-2/src/Components/ProfileList.tsx
+++ Practical-2/src/Components/ProfileList.tsx
@@ -1,9 +1,25 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {CharType} from '../interface';
+import {fetchCharacters} from '../services/index';
 
 const ProfileList: FC = () => {
+    const [profiles, setProfiles] = useState<CharType[]>([]);
+    useEffect(() => {
+        const fetchApi = async () => {
+            setProfiles(await fetchCharacters());
+        };
+        fetchApi();
+    }, []);
     return (
-        <div>
-            <h1>About</h1>
+        <div className="cards">
+            {profiles.slice(0, 12).map((profile) => (
+                <div key={profile.id}>
+                    <div className="profile-card">
+                        <img src={profile.image} alt="profile" />
+                        <h2>{profile.name}</h2>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
