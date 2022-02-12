f3be6a0aef46e9f82eb89c7daabd7ec2e4912052
Akash52 <ac8572611@gmail.com>
2022-02-12 17:35:22 +0530

    Intefaces

---
+++ Practical-2/src/interface/index.tsx
@@ -0,0 +1,33 @@
+export interface IPInfo {
+    ip: string;
+    version: string;
+    city: string;
+    country_name: string;
+    region: string;
+    region_code: number;
+    country_capital: string;
+    country: string;
+    timezone: string;
+    postal: number;
+    country_area: number;
+    country_population: number;
+    org: string;
+    latitude: number;
+}
+
+export interface CharType {
+    id: number;
+    name: string;
+    total: string;
+    email: string;
+    phone: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+    website: string;
+    company: string;
+    avatar: string;
+    image: string;
+}
