29f0f692de47870fee021a766d11989580cc8712
Akash52 <ac8572611@gmail.com>
2022-02-13 03:10:18 +0530

    About page done

--- Practical-2/src/Pages/About.tsx
+++ Practical-2/src/Pages/About.tsx
@@ -2,8 +2,18 @@ import React, {FC} from 'react';
 
 const About: FC = () => {
     return (
-        <div>
-            <h1>Help</h1>
+        <div className="container">
+            <h1>About</h1>
+            <h3 className="about">
+                <strong>In </strong> these project we implemented major React
+                concept like React Components, <strong> Hooks </strong> , State
+                Management , Prop Passing , React Router , React-icons for
+                create our UI , we handle <strong>404</strong> error using
+                NotFound page & also we called 2 API 1st one is{' '}
+                <strong>IP Info </strong> Provide current device IP information
+                like region , city , country etc. & 2nd one is a simply provide{' '}
+                <strong> Harry Potter </strong> cast details .
+            </h3>
         </div>
     );
 };
