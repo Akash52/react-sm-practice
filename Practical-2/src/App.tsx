2afde997346e411993a4c16eb69b344ed9de0d21
Akash52 <ac8572611@gmail.com>
2022-02-13 18:22:27 +0530

    we avoid multiple import in app.tsx

--- Practical-2/src/App.tsx
+++ Practical-2/src/App.tsx
@@ -1,10 +1,7 @@
 import React, {FC} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import NotFound from './Pages/NotFound';
-import Profile from './Pages/Profile';
 import Navbar from './Components/Navbar';
+import {About, NotFound, Profile, Home} from './Pages';
 
 const App: FC = () => {
     return (
