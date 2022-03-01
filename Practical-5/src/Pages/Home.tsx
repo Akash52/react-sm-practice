33ebe557ea1cb21aa68a94870e61756198e3f76d
Akash52 <ac8572611@gmail.com>
2022-03-02 01:31:46 +0530

    Home component path changed

--- Practical-5/src/Pages/Home.tsx
+++ Practical-5/src/Pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FC} from 'react';
-import UsersList from '../Components/Users/UsersList';
+import UsersList from '../Components/Users/UserList';
 
 const Home: FC = () => {
     return <UsersList />;
