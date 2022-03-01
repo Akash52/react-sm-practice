76b35cc18a4217f5b15de5833425cdadbeab0a6f
Akash52 <ac8572611@gmail.com>
2022-03-02 01:29:53 +0530

    Table heade changes made

--- Practical-5/src/Components/Users/Table/TableHeader.tsx
+++ Practical-5/src/Components/Users/Table/TableHeader.tsx
@@ -4,29 +4,26 @@ const TableHeader: FC = () => {
     return (
         <>
             {/* Table Header */}
-            <thead className="bg-gray-50">
+            <thead className="bg-gray-50 ">
                 <tr>
                     <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-semibold text-gray-600  tracking-wider">
+                        className="px-8 py-3 text-left text-xs font-semibold text-gray-600  tracking-wider">
                         Name
                     </th>
                     <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-semibold  text-gray-600  tracking-wider">
+                        className="px-8 py-3 text-left text-xs font-semibold  text-gray-600  tracking-wider">
                         Status
                     </th>
                     <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-semibold  text-gray-600  tracking-wider">
+                        className="px-8 py-3 text-left text-xs font-semibold  text-gray-600  tracking-wider">
                         Access
                     </th>
                     <th
                         scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-600  tracking-wider"></th>
-                    <th scope="col" className="relative px-6 py-3">
-                        <span className="sr-only">Edit</span>
-                    </th>
+                        className="px-8 py-3 text-left text-xs font-medium text-gray-600  tracking-wider"></th>
                 </tr>
             </thead>
         </>
